388dce84d8f254daa9983760e6b6d45066ec46b3
Tom Lee <thomasslee97@gmail.com>
2020-08-10 15:58:57 +0100

    fix: Mongo update script

--- scripts/upgrade-mongo.sh
+++ scripts/upgrade-mongo.sh
@@ -6,15 +6,17 @@ docker stop sisyfos
 docker stop mongo
 docker rm mongo
 docker run --network sofie --name=mongo --hostname=mongodb --volume=/opt/mongo/db:/data/db --volume=/opt/mongo/backup:/data/backup --volume=/etc/timezone:/etc/timezone:ro -p 127.0.0.1:27017:27017 --detach=true mongo:3.6.18 mongod --replSet rs0
-sleep 5
+sleep 30
 docker exec -it mongo mongo --eval  'db.adminCommand( { setFeatureCompatibilityVersion: "3.6" } )'
 docker stop mongo
 docker rm mongo
-docker run --network sofie --name=mongo --hostname=mongodb --volume=/opt/mongo/db:/data/db --volume=/opt/mongo/backup:/data/backup --volume=/etc/timezone:/etc/timezone:ro -p 127.0.0.1:27017:27017 --detach=true mongo:4.0.18 mongod --replSet rs0
-sleep 5
+docker run --restart always --network sofie --name=mongo --hostname=mongodb --volume=/opt/mongo/db:/data/db --volume=/opt/mongo/backup:/data/backup --volume=/etc/timezone:/etc/timezone:ro -p 127.0.0.1:27017:27017 --detach=true mongo:4.0.18 mongod --replSet rs0
+sleep 30
 docker exec -it mongo mongo --eval  'db.adminCommand( { setFeatureCompatibilityVersion: "4.0" } )'
+docker exec -it mongo mongo --eval  'db.adminCommand( { replSetStepDown: 30, secondaryCatchUpPeriodSecs: 10, force: true } )'
+docker restart mongo
 docker stop mongo
 docker rm mongo
 docker run --restart always --network sofie --name=mongo --hostname=mongodb --volume=/opt/mongo/db:/data/db --volume=/opt/mongo/backup:/data/backup --volume=/etc/timezone:/etc/timezone:ro -p 127.0.0.1:27017:27017 --detach=true mongo:4.2.7 mongod --replSet rs0
-sleep 5
+sleep 30
 docker exec -it mongo mongo --eval  'db.adminCommand( { setFeatureCompatibilityVersion: "4.2" } )'
