3a1b69abbc05e92e3adf481db1aa00c2e91bce8c
Tom Lee <thomasslee97@gmail.com>
2020-08-10 16:12:12 +0100

    fix: run upgrade again

--- scripts/upgrade-mongo.sh
+++ scripts/upgrade-mongo.sh
@@ -20,3 +20,5 @@ docker rm mongo
 docker run --restart always --network sofie --name=mongo --hostname=mongodb --volume=/opt/mongo/db:/data/db --volume=/opt/mongo/backup:/data/backup --volume=/etc/timezone:/etc/timezone:ro -p 127.0.0.1:27017:27017 --detach=true mongo:4.2.7 mongod --replSet rs0
 sleep 30
 docker exec -it mongo mongo --eval  'db.adminCommand( { setFeatureCompatibilityVersion: "4.2" } )'
+docker exec -it mongo mongo --eval  'db.adminCommand( { replSetStepDown: 30, secondaryCatchUpPeriodSecs: 10, force: true } )'
+docker restart mongo
